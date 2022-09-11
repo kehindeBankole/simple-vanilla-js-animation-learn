7776ea216d49fefb921aa850f7433dd380d9b38b
kehindeBankole <bankolek1@gmail.com>
2022-09-11 02:26:37 +0100

    commit

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const ad = document.getElementsByClassName(`navigation-link`);
 const ads = document.getElementsByClassName(`project-preview`)[0];
 new Array(...ad).map((d) => {
   d.addEventListener("mouseover", () => {
-    // console.log(d);
     ads.style.backgroundImage = `url(img-${d.dataset.num}.jpeg)`;
-    console.log(`url(img-${d.dataset.num}.jpeg)`);
     tl.to(".project-preview", 1, {
       width: "600px",
       ease: Expo.easeInOut,
